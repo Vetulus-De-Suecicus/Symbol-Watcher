c183430880f2dbb441935d752546517a382f9aba
Vetulus-De-Suecicus <vetulusmontanis@gmail.com>
2025-05-09 13:18:37 +0200

    Update stockplot.py
    removed unused code

--- stockplot.py
+++ stockplot.py
@@ -1,12 +1,10 @@
-# MIT LICENSE 2025
-import matplotlib.pyplot as plt
+# MIT LICENSE 2025import matplotlib.pyplot as plt
 import yfinance as yf
 from matplotlib.animation import FuncAnimation
 
-### ADD YOUR STOCKS AND HOLDINGS HERE
 holdings = {"MSFT": 57,
-           "AAPL": 161}
-
+           "AAPL": 161,
+           "IPCO.ST": 0}
 
 def update_graphs(_):
     total_price = 0
@@ -25,17 +23,14 @@ def update_graphs(_):
         current_ax.plot(datetime_index, opening, label="Open", color="blue")
         current_ax.fill_between(datetime_index, low, high, color="orange", alpha=0.3, label="High-Low interval")
         current_ax.fill_between(datetime_index, closing, opening, color="red", alpha=0.3, label="Close-Open interval")
-        if ticker in holdings:
-            total_price += closing.iloc[-1] * holdings[ticker]
-            current_price = closing.iloc[-1] * holdings[ticker]
-            current_ax.set_title(f"{ticker} : Close: {closing.iloc[-1]:.2f} SEK : {current_price:.2f}", fontsize=10)
-        else:
-            current_ax.set_title(f"{ticker} : Close: {closing.iloc[-1]:.2f} SEK", fontsize=10)
+        total_price += closing.iloc[-1] * holdings[ticker]
+        current_price = closing.iloc[-1] * holdings[ticker]
+        current_ax.set_title(f"{ticker} : Close: {closing.iloc[-1]:.2f} : {current_price:.2f}", fontsize=10)
         current_ax.grid(True)
         ax2 = current_ax.twinx()
         ax2.bar(datetime_index, volume, color="grey", alpha=0.5, label="Volume", width=0.001)
         ax2.set_ylabel("Volume")
-    fig.suptitle(f"Total Price: {total_price:.2f} SEK", fontsize=12)
+    fig.suptitle(f"Total Value: {total_price:.2f}", fontsize=12)
 
 if __name__ == "__main__":
     if len(holdings) == 1:

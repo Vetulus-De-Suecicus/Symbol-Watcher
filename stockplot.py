5421156f11a46f5a4d4bc1508af5e1c921c6f5d2
Vetulus-De-Suecicus <vetulusmontanis@gmail.com>
2025-05-15 13:12:31 +0200

    Update stockplot.py
    Added the ability to automatically add another column of graphs if there are more than a certain amount of symbols present.
    
    Added the ability to easy configure the plots to ones own liking.
    
    Added the ability to change the update intervall more easily

--- stockplot.py
+++ stockplot.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
 import yfinance as yf
 from matplotlib.animation import FuncAnimation
+import math
 
-### insert your holdings here ###
-############ {"TICKER": [AMOUNT, BUYING PRICE]} #############
-holdings = {"MSFT": [1, 500],
-           "AAPL": [1, 59.99],
-           "^OMX": [0, 0]}
+### plot colours ###
+opencolour="red"
+closecolour="green"
+hilointcolour="orange"
+closeopenintcolour="red"
+volumecolour="grey"
+updateinterval=120000 # 60seconds = 60000
+
+### add your holdings. amount and price of specific symbols ###
+### "SYMBOL": [AMOUNT, PRICE] ###
+holdings = {"SAAB-B.ST": [1, 500],
+           "SSAB-B.ST": [1, 500],
+           "^OMX": [0, 0],
+           "MSFT": [1,500],
+           "AAPL": [5,100],
+           "AMZN": [1, 100]}
 
 def update_graphs(_):
     total_value = 0
@@ -22,10 +34,10 @@ def update_graphs(_):
         volume = history['Volume']
         current_ax = axes[idx]
         current_ax.clear()
-        current_ax.plot(datetime_index, opening, label="Open", color="red")
-        current_ax.plot(datetime_index, closing, label="Close", color="green")
-        current_ax.fill_between(datetime_index, low, high, color="orange", alpha=0.3, label="High-Low interval")
-        current_ax.fill_between(datetime_index, closing, opening, color="red", alpha=0.3, label="Close-Open interval")
+        current_ax.plot(datetime_index, closing, label="Close", color=closecolour)
+        current_ax.plot(datetime_index, opening, label="Open", color=opencolour)
+        current_ax.fill_between(datetime_index, low, high, color=hilointcolour, alpha=0.3, label="High-Low interval")
+        current_ax.fill_between(datetime_index, closing, opening, color=closeopenintcolour, alpha=0.3, label="Close-Open interval")
         amountholding, price = holdings[ticker]
         total_value += closing.iloc[-1] * amountholding
         current_total = closing.iloc[-1] * amountholding
@@ -34,21 +46,32 @@ def update_graphs(_):
         previous_close = history['Close'].iloc[0]
         difference = closing.iloc[-1] - previous_close
         percentage_change = (difference / previous_close) * 100
-        current_ax.set_title(f"{ticker} : Close: {closing.iloc[-1]:.2f} : {current_total:.2f} : Diff: {difference:.2f} ({percentage_change:.2f}%) : {purchased_difference:.2f}", fontsize=10)
+        current_ax.set_title(f"{ticker} : Close: {closing.iloc[-1]:.2f} : {current_total:.2f} : Diff: {difference:.2f} ({percentage_change:.2f}%) : Purch. Diff. {purchased_difference:.2f}", fontsize=10)
         current_ax.grid(True)
         ax2 = current_ax.twinx()
-        ax2.bar(datetime_index, volume, color="grey", alpha=0.5, label="Volume", width=0.001)
+        ax2.bar(datetime_index, volume, color=volumecolour, alpha=0.5, label="Volume", width=0.001)
         ax2.set_ylabel("Volume")
     fig.suptitle(f"Total Value: {total_value:.2f} : Change: {total_valchange:.2f}", fontsize=12)
 
 if __name__ == "__main__":
+    print(f"""
+          Open Colour: {opencolour}
+          Close Colour: {closecolour}
+          High-Low Interval Colour: {hilointcolour}
+          Close-Open Interval Colour: {closeopenintcolour}
+          Volume Colour: {volumecolour}
+""")
     if len(holdings) == 1:
         fig, axes_obj = plt.subplots(1, 1)
         axes = [axes_obj]
+    elif len(holdings) >4:
+        graphnrow = ((math.ceil(len(holdings)/2)))
+        fig, axes = plt.subplots(graphnrow, 2, squeeze=False)
+        axes = axes.flatten()
     else:
         fig, axes = plt.subplots(len(holdings), 1, squeeze=False)
         axes = axes.flatten()
     fig.canvas.manager.set_window_title("Symbol Watcher")
-    ani = FuncAnimation(fig, update_graphs, interval=120000, cache_frame_data=False)
+    ani = FuncAnimation(fig, update_graphs, interval=updateinterval, cache_frame_data=False)
     plt.tight_layout(h_pad=1, w_pad=1, rect=[0, 0.05, 0.95, 0.9])
     plt.show()
